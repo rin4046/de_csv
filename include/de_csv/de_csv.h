ccb26835db208b9d99791f030d5e1c48491f0781
rin4046 <rin@rinrin.me>
2022-08-14 03:27:17 +0900

    Add <=C++17 support

--- include/de_csv/de_csv.h
+++ include/de_csv/de_csv.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#if __cplusplus >= 202002L
+
 #include <concepts>
 #include <istream>
 #include <optional>
@@ -97,3 +99,106 @@ private:
   }
 };
 } // namespace de_csv
+
+#else
+
+#include <istream>
+#include <optional>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <vector>
+
+namespace de_csv {
+template <typename T>
+using FromVector =
+    std::enable_if_t<std::is_constructible_v<T, std::vector<std::string> &&>,
+                     std::optional<T>>;
+
+template <typename T>
+using FromUnorderedMap = std::enable_if_t<
+    std::is_constructible_v<T, std::unordered_map<std::string, std::string> &&>,
+    std::optional<T>>;
+
+template <typename S> class CsvReader {
+  S stream_;
+  std::vector<std::string> header_;
+  const bool has_header_;
+
+public:
+  // streamをmoveして使う
+  CsvReader(S &&stream, bool has_header = true)
+      : stream_(std::move(stream)), has_header_(has_header) {
+    if (has_header_) {
+      std::string buf;
+      std::getline(stream_, buf);
+      header_ = tokenize_(buf);
+    }
+  }
+
+  CsvReader(S &&stream, std::vector<std::string> &&header)
+      : stream_(std::move(stream)),
+        header_(std::forward<std::vector<std::string>>(header)),
+        has_header_(true) {}
+
+  template <typename T> std::vector<FromVector<T>> deserialize() {
+    std::string buf;
+    std::vector<FromVector<T>> records;
+    while (std::getline(stream_, buf)) {
+      try {
+        records.emplace_back(T{tokenize_(buf)});
+      } catch (...) {
+        records.emplace_back(std::nullopt);
+      }
+    }
+    return records;
+  }
+
+  template <typename T> std::vector<FromUnorderedMap<T>> deserialize() {
+    std::string buf;
+    std::vector<FromUnorderedMap<T>> records;
+    while (std::getline(stream_, buf)) {
+      try {
+        std::unordered_map<std::string, std::string> res;
+        auto tmp = tokenize_(buf);
+        for (size_t i = 0; i < header_.size(); i++) {
+          res.emplace(header_.at(i), tmp.at(i));
+        }
+        records.emplace_back(T{std::move(res)});
+      } catch (...) {
+        records.emplace_back(std::nullopt);
+      }
+    }
+    return records;
+  }
+
+  const std::vector<std::string> &header() {
+    return header_;
+  }
+
+private:
+  // CSVの各行をtokenize
+  std::vector<std::string> tokenize_(const std::string &str) {
+    if (str.empty()) {
+      return {};
+    }
+
+    std::vector<std::string> tokens;
+    std::string::size_type l = 0;
+
+    while (1) {
+      auto r = str.find(',', l);
+      if (r == std::string::npos) {
+        tokens.emplace_back(str.begin() + l, str.end());
+        break;
+      }
+      tokens.emplace_back(str.begin() + l, str.begin() + r);
+      l = r + 1;
+    }
+
+    return tokens;
+  }
+};
+} // namespace de_csv
+
+#endif
